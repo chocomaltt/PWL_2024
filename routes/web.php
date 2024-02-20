641e2828e74393f87a32772327d142d0b9aed1a5
Reza Arya Wijaya <wijayarezaarya@gmail.com>
2024-02-20 10:19:30 +0700

    Mod : Route Parameters

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,15 @@ Route::get('/world', function(){
 Route::get('/about', function($nim = "2241720252", $nama = "Reza Arya Wijaya"){
     return "About Me <br> NIM : " . $nim . "<br> Nama : " . $nama;
 });
+
+Route::get('/user/{name}', function($name){
+    return 'Nama saya ' . $name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId){
+    return 'Pos ke-' . $postId . ' Komentar ke-' . $commentId;
+});
+
+Route::get('/articles/{id}', function($id){
+    return "Halaman Artikel dengan ID {$id}";
+});
